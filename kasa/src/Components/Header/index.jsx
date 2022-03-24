e058d8d74a879ed2d146b9b482b153b7e0a50868
jean-christophe Becker <mothos@hotmail.fr>
2022-03-24 11:58:13 +0100

    petites modification de dernières minutes

--- kasa/src/Components/Header/index.jsx
+++ kasa/src/Components/Header/index.jsx
@@ -1,31 +1,27 @@
 import './Header.scss'
-import { Link, BrowserRouter } from 'react-router-dom'
+
 import Logo from '../../Assets/logo.svg'
 
 function Header() {
     return (
         <nav className="nav" data-testid="headerNav">
-            <BrowserRouter>
-                <Link to="/home">
-                    <img
-                        src={Logo}
-                        className="nav__img"
-                        alt=""
-                        data-testid="logo"
-                    />
-                </Link>
-            </BrowserRouter>
+            <a href="/">
+                <img
+                    src={Logo}
+                    className="nav__img"
+                    alt=""
+                    data-testid="logo"
+                />
+            </a>
+
             <div data-testid="headerMenu">
-                <BrowserRouter>
-                    <Link to="/home" className="nav__link">
-                        Accueil
-                    </Link>
-                </BrowserRouter>
-                <BrowserRouter>
-                    <Link to="/apropos" className="nav__link">
-                        À Propos
-                    </Link>
-                </BrowserRouter>
+                <a href="/" className="nav__link">
+                    Accueil
+                </a>
+
+                <a href="/apropos" className="nav__link">
+                    À Propos
+                </a>
             </div>
         </nav>
     )
